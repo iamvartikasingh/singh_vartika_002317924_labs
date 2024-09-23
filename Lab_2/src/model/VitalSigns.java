79a543cbc5e2e10058343ad553bcb548de42e0a4
vartika <singh.vart@northeastern.edu>
2024-09-22 21:53:54 -0400

    Lab 2 commit

---
+++ Lab_2/src/model/VitalSigns.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author vartika
+ */
+public class VitalSigns {
+      String date;
+  double bloodPressure;
+  double temprature;
+  int pulse;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getBloodPressure() {
+        return bloodPressure;
+    }
+
+    public void setBloodPressure(double bloodPressure) {
+        this.bloodPressure = bloodPressure;
+    }
+
+    public double getTemprature() {
+        return temprature;
+    }
+
+    public void setTemprature(double temprature) {
+        this.temprature = temprature;
+    }
+
+    public int getPulse() {
+        return pulse;
+    }
+
+    public void setPulse(int pulse) {
+        this.pulse = pulse;
+    }
+    
+    @Override
+    public String toString(){
+        return getDate();
+    }
+}
